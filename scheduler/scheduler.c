d0447761b7c19c1cc3a9cfc742b66e766f949ba0
Kaique Carvalho <kaiquecarvalho479jogos@gmail.com>
2025-04-21 08:43:39 -0300

    Fix compilation issue on Windows using CLion

--- scheduler/scheduler.c
+++ scheduler/scheduler.c
@@ -26,16 +26,16 @@ void input_processes(Process processes[], int *n, int *quantum)
 {
     do
     {
-        printf("Digite o número de processos: ");
+        printf("Digite o numero de processos: ");
         scanf("%d", n);
 
         if (*n <= 0)
         {
-            printf("O número de processos deve ser maior que zero. Tente novamente.\n");
+            printf("O numero de processos deve ser maior que zero. Tente novamente.\n");
         }
         else if (*n > MAX_PROCESSES)
         {
-            printf("Número máximo de processos excedido. Limitando a %d processos.\n", MAX_PROCESSES);
+            printf("Numero máximo de processos excedido. Limitando a %d processos.\n", MAX_PROCESSES);
             *n = MAX_PROCESSES;
         }
     } while (*n <= 0);
@@ -54,12 +54,12 @@ void input_processes(Process processes[], int *n, int *quantum)
     for (int i = 0; i < *n; i++)
     {
         processes[i].id = i + 1;
-        printf("Digite o tempo de execução do processo P%d: ", i + 1);
+        printf("Digite o tempo de exeexecucaocução do processo P%d: ", i + 1);
         scanf("%d", &processes[i].burst_time);
 
         if (processes[i].burst_time <= 0)
         {
-            printf("Tempo de execução não pode ser <= 0. Definindo como 1.\n");
+            printf("Tempo de execucao não pode ser <= 0. Definindo como 1.\n");
             processes[i].burst_time = 1;
         }
 
@@ -173,9 +173,9 @@ void display_metrics(Process processes[], int n)
 {
     float total_waiting_time = 0, total_turnaround = 0, total_burst_time = 0;
 
-    printf("\nMétricas dos Processos:\n");
+    printf("\nMetricas dos Processos:\n");
     printf("+----+----------------+----------------+----------------+\n");
-    printf("| ID | Tempo Espera   | Tempo Retorno  | Tempo Execução |\n");
+    printf("| ID | Tempo Espera   | Tempo Retorno  | Tempo Execucao |\n");
     printf("+----+----------------+----------------+----------------+\n");
 
     for (int i = 0; i < n; i++)
@@ -192,8 +192,8 @@ void display_metrics(Process processes[], int n)
     }
 
     printf("+----+----------------+----------------+----------------+\n");
-    printf("\nTempo médio de espera: %.2f\n", total_waiting_time / n);
-    printf("Tempo médio de retorno: %.2f\n", total_turnaround / n);
+    printf("\nTempo medio de espera: %.2f\n", total_waiting_time / n);
+    printf("Tempo medio de retorno: %.2f\n", total_turnaround / n);
     printf("Tempo total de uso do processador: %.2f\n", total_burst_time);
 }
 
